35fe6bbff8c359aa479887745a1fe8803acaeabd
hedilarbi <hedi.larbi@esprit.tn>
2024-11-08 15:34:55 +0100

    reverse orders

--- services/ordersServices/getOrdersService.js
+++ services/ordersServices/getOrdersService.js
@@ -2,7 +2,7 @@ const Order = require("../../models/Order");
 
 const getOrdersService = async () => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().sort({ createdAt: -1 });
     return { response: orders };
   } catch (err) {
     return { error: err.message };

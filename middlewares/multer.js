5dc6fa5b3063d8c6573123584fa23784fbb3be6a
hedilarbi <hedi.larbi@esprit.tn>
2025-01-22 23:49:28 +0100

    test

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const Multer = multer({
   storage: multer.memoryStorage(),
-  limits: 2 * 1024 * 1024,
+  limits: 10 * 1024 * 1024,
 });
 
 module.exports = Multer;
